d1054df113d4081bff6cddb225fb1b3b54567f77
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2022-11-22 08:41:05 +0100

    feat: add NavigationButtonGroup

---
+++ MediaManager/MediaManager/GUI/Atoms/NavigationButtonGroup.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace MediaManager.GUI.Atoms
+{
+    /// <summary>
+    /// Interaction logic for NavigationButtonGroup.xaml
+    /// </summary>
+    [ContentProperty("Children")]
+    public partial class NavigationButtonGroup : UserControl
+    {
+        public Orientation Orientation { get; set; } = Orientation.Horizontal;
+        public ObservableCollection<NavigationButton> Children { get; private set; } = new ObservableCollection<NavigationButton>();
+
+        public NavigationButtonGroup()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            Children.CollectionChanged += new System.Collections.Specialized.NotifyCollectionChangedEventHandler(Children_CollectionChanged);
+        }
+
+        void Children_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case System.Collections.Specialized.NotifyCollectionChangedAction.Add:
+                    foreach (NavigationButton elem in e.NewItems) container.Children.Add(elem);
+                    break;
+                case System.Collections.Specialized.NotifyCollectionChangedAction.Remove:
+                    foreach (NavigationButton elem in e.OldItems) container.Children.Remove(elem);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
