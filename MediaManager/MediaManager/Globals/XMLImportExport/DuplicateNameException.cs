82f01aff9f501bba0eb6e9d18531b248a5585cc9
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2022-11-23 19:49:01 +0100

    feat: add XMLImportExport

---
+++ MediaManager/MediaManager/Globals/XMLImportExport/DuplicateNameException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MediaManager.Globals.XMLImportExport
+{
+    /// <summary>
+    /// Indicates, that a name of an entry is duplicated.
+    /// </summary>
+    public class DuplicateNameException : FormatException
+    {
+        public DuplicateNameException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
