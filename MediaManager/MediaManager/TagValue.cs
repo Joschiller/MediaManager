0059c4c1196a2374e8c444b5834c1c1b4d42dcbc
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2022-11-25 06:27:00 +0100

    feat: add TagList

---
+++ MediaManager/MediaManager/TagValue.cs
@@ -0,0 +1,9 @@
+﻿namespace MediaManager
+{
+    public enum TagValue
+    {
+        Negative = -1,
+        Neutral = 0,
+        Positive = 1,
+    };
+}
---
+++ MediaManager/MediaManager/ValuedTag.cs
@@ -0,0 +1,13 @@
+﻿namespace MediaManager
+{
+    public class ValuedTag
+    {
+        public Tag Tag { get; set; }
+        private TagValue _Value;
+        public bool? Value
+        {
+            get => _Value == TagValue.Negative ? false : (_Value == TagValue.Positive ? true : (bool?)null);
+            set => _Value = !value.HasValue ? TagValue.Neutral : (value.Value ? TagValue.Positive : TagValue.Negative);
+        }
+    }
+}
