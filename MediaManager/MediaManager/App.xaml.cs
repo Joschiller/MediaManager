5adb53c9189c8d45cfdfda62fd57058c9212b014
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2023-04-09 13:26:00 +0200

    feat: Global Exception Handler

--- MediaManager/MediaManager/App.xaml.cs
+++ MediaManager/MediaManager/App.xaml.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MediaManager.Globals.DefaultDialogs;
+using System;
 using System.Windows;
+using static MediaManager.Globals.LanguageProvider.LanguageProvider;
+using static MediaManager.Globals.Navigation;
 
 namespace MediaManager
 {
@@ -13,5 +11,25 @@ namespace MediaManager
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException; ;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            var defaultEx = "An unexpected error occurred. Please close and restart the application!\nYou can take a screenshot to restore your data later on or to report the error.";
+            try
+            {
+                var readEx = getString("Common.DispatcherUnhandledException");
+                if (string.IsNullOrEmpty(readEx)) throw new Exception();
+                ShowDefaultDialog(readEx, SuccessMode.Error);
+            }
+            catch (Exception)
+            {
+                ShowDefaultDialog(defaultEx, SuccessMode.Error);
+            }
+            e.Handled = true;
+        }
     }
 }
