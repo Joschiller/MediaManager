f5d1704cdd8d6883bf2ff52e579dbba6ae462ef5
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2023-04-20 17:12:53 +0200

    feat: add CheckedList

---
+++ MediaManager/MediaManager/GUI/Atoms/CheckedList.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MediaManager.GUI.Atoms
+{
+    /// <summary>
+    /// Interaction logic for CheckedList.xaml
+    /// </summary>
+    public partial class CheckedList : UserControl
+    {
+        public delegate void CheckedListItemHandler(CheckedListItem item);
+        public event CheckedListItemHandler SelectionChanged;
+        public delegate void CheckedListItemListHandler(List<CheckedListItem> item);
+        public event CheckedListItemListHandler CheckedChanged;
+
+        public ObservableCollection<CheckedListItem> Items { get; set; } = new ObservableCollection<CheckedListItem>();
+        private List<int> checkedItemIds = new List<int>();
+
+        public CheckedList()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public void SetItems(List<CheckedListItem> items)
+        {
+            Items.Clear();
+            checkedItemIds.Clear();
+            items.ForEach(Items.Add);
+        }
+        public List<CheckedListItem> GetCheckedItems() => Items.Where(i => checkedItemIds.Contains(i.Id)).ToList();
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            checkedItemIds.RemoveAll(i => i == (int)((CheckBox)sender).Tag);
+            CheckedChanged?.Invoke(GetCheckedItems());
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            checkedItemIds.Add((int)((CheckBox)sender).Tag);
+            CheckedChanged?.Invoke(GetCheckedItems());
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var id = (int)((CheckBox)sender).Tag;
+            SelectionChanged?.Invoke(Items.FirstOrDefault(i => i.Id == id));
+        }
+    }
+}
---
+++ MediaManager/MediaManager/GUI/Atoms/CheckedListItem.cs
@@ -0,0 +1,8 @@
+﻿namespace MediaManager.GUI.Atoms
+{
+    public class CheckedListItem
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
