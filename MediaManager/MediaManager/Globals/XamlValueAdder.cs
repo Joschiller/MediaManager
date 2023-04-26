c99b46d15ea765f733075696a484343622d95348
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2023-04-26 18:16:12 +0200

    feat(XamlValueAdder): set default value

--- MediaManager/MediaManager/Globals/XamlValueAdder.cs
+++ MediaManager/MediaManager/Globals/XamlValueAdder.cs
@@ -6,7 +6,7 @@ namespace MediaManager.Globals
 {
     public class XamlValueAdder : IValueConverter
     {
-        public double Pad { get; set; }
+        public double Pad { get; set; } = 0;
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => ParseDouble(value, 0) + Pad;
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => ParseDouble(value, 0) - Pad;
