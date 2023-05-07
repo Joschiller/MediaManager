3fa31f030bf486185597be740789d1a754434aaf
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2023-05-07 18:51:37 +0200

    feat(Pager): configurable text placement

--- MediaManager/MediaManager/GUI/Components/Pager.xaml.cs
+++ MediaManager/MediaManager/GUI/Components/Pager.xaml.cs
@@ -35,6 +35,11 @@ namespace MediaManager.GUI.Components
                 UpdateGUI();
             }
         }
+        /// <summary>
+        /// Placement of the page and item counter within the control.<br/>
+        /// By default, <see cref="Dock.Top"/> will be used.
+        /// </summary>
+        public Dock TextPlacement { get => DockPanel.GetDock(textPanel); set => DockPanel.SetDock(textPanel, value); }
         #endregion
 
         #region Setup
